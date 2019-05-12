11bf16048082f2a73afcddd0a82e17894d3557aa
tasertasertaser <40524078+tasertasertaser@users.noreply.github.com>
2019-05-11 22:15:55 -0700

    Update ft_base.c

--- ft_base.c
+++ ft_base.c
@@ -16,9 +16,9 @@
 **                          10 11 12 13 14 15
 **
 **	Converting 348 to Hexadecimal
-**                  remainder
-**                  ▾   symbol (reverse these!)
-**                  ▾   ▾
+**                      remainder
+**                      ▾   symbol (reverse these!)
+**                      ▾   ▾
 **	348 / 16 = 21 | 12  C
 **	 21 / 16 =  1 | 5   5
 **	  1 / 16 =  0 | 1   1
